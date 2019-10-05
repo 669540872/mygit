ab41e5b4898b5251c5ae44821f35e19fa5c7dd25
669540872 <669540872@qq.com>
2019-10-05 11:04:47 +0800

    测试包

---
+++ PackageText/src/PackageText.java
@@ -0,0 +1,11 @@
+import course.*;
+import student.*;
+import teacher.*;
+
+public class PackageText{
+	public static void main(String[] agrs){
+		Student st = new Student();
+		Teacher te = new Teacher();
+		Course cs = new Course();
+	}
+}
