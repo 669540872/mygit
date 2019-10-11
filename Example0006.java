f2bf89e3bc331ee63e8b093d75a328a6a5503323
669540872 <669540872@qq.com>
2019-10-11 22:56:11 +0800

    非输入式字符数组类

---
+++ Example0006.java
@@ -0,0 +1,10 @@
+
+public class Example0006 {
+    public static void main(String[] args) {
+    	String[] goods = new String[] {"Nice背包","外套","运动鞋","T恤","裤子"};
+    	System.out.println("输出这次产品为：");
+    	for(int i=0;i<goods.length;i++) {
+    		System.out.print("\t"+goods[i]);
+    	}
+    }
+}
